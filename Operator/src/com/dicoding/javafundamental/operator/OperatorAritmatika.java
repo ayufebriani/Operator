3390b6f3dff63515d24aab396e08a4bace528329
ayufebriani <ayufebriani879@gmail.com>
2020-11-10 23:11:38 +0700

    publish file index

---
+++ Operator/src/com/dicoding/javafundamental/operator/OperatorAritmatika.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.operator;
+
+/**
+ *
+ * @author ASUS
+ */
+public class OperatorAritmatika {
+      public static void main(String[] args) {
+        System.out.println("Operasi Penjumlahan");
+        int hasilPenjumlahan = 5 + 1;
+        System.out.println("Hasil 7 + 1 = " + hasilPenjumlahan);
+        System.out.println();
+        System.out.println("Operasi Pengurangan");
+        int hasilPengurangan = 4 - 1;
+        System.out.println("Hasil 4 - 2 = " + hasilPengurangan);
+        System.out.println();
+        System.out.println("Operasi Pengalian");
+        int hasilPengalian = 5 * 5;
+        System.out.println("Hasil 8 * 5 = " + hasilPengalian);
+        System.out.println();
+        System.out.println("Operasi Pembagian");
+        int hasilPembagian = 20 / 2;
+        System.out.println("Hasil 20 / 10 = " + hasilPembagian);
+        System.out.println();
+        System.out.println("Operasi Habis bagi");
+        int hasilSisa = 8 % 2;
+        System.out.println("Hasil 8 % 2 = " + hasilSisa);
+        int hasilSisaLain = 9 % 2;
+        System.out.println("Hasil 9 % 2 = " + hasilSisaLain);
+        System.out.println();
+    }
+}
